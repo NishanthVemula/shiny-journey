a18c2c851fd0f2f614173b6a7668e94b934bcdc5
Vemula Nishanth <93464585+NishanthVemula@users.noreply.github.com>
2022-12-15 17:12:11 +0530

    [ Udemy / LP 01 / C 1 ] Section 5: Arrays - Comparision

---
+++ Udemy/LearningPaths/LP_01/ModernJSBootcamp2022/Sections/S02/demoArray.js
@@ -0,0 +1,182 @@
+function demoArray1() {
+    console.group("Create and Access")
+
+    let emptyArray = []
+    console.log(`$ let emptyArray = []`)
+    console.log(`\temptyArray = `, emptyArray)
+
+    let stringArray = ["String1", "String2", "String3"]
+    console.log(`$ let stringArray = ["String1", "String2", "String3"]`)
+    console.log(`\tstringArray = `, stringArray)
+
+    let numberArray = [1, 2, 3, 4, 9, 13]
+    console.log(`$ let numberArray = [1, 2, 3, 4, 9 ,13]`)
+    console.log(`\tnumberArray = `, numberArray)
+
+    let someArray = ["string", 123, "1324", true, null, undefined, NaN, Infinity, 0, 'c']
+    console.log(`$ let someArray = [ "string", 123, "1324", true, null, undefined, NaN, Infinity, 0, 'c']`)
+    console.log(`\tsomeArray = `, someArray)
+
+    console.log(`- looping through someArray`)
+    someArray.forEach((element, index) => {
+        console.log(`\tindex\t\t`, index,`
+        element\t\t`, element,`
+        someArray[${index}]\t`, someArray[index])
+        // console.log(`\tindex\t\t`, index)
+        // console.log(`\telement\t\t`, element)
+        // console.log(`\tsomeArray[${index}]\t`, someArray[index])
+    });
+
+    console.groupEnd()
+}
+
+function demoArray2() {
+    console.group("Compare")
+
+
+    let someArray = ['red', 'green', 'blue']
+    console.log(`$ let someArray = ['red', 'green', 'blue']`)
+    console.log(`\tsomeArray = `, someArray)
+
+    let newArray = ['red', 'green', 'blue']
+    console.log(`$ let newArray = ['red', 'green', 'blue']`)
+    console.log(`\tnewArray = `, newArray)
+
+    console.log(`$ someArray == newArray\n\t${someArray == newArray}`)
+
+    console.groupEnd()
+}
+
+function demoArray3() {
+    console.group("Push & Pop")
+
+    const someArray = ["string", 123, "1324", true, null, undefined, NaN, Infinity, 0, 'c']
+    console.log(`$ const someArray = [ "string", 123, "1324", true, null, undefined, NaN, Infinity, 0, 'c']`)
+    console.log(`\tsomeArray.toString() = `, someArray.toString())
+    console.log(`\tsomeArray = `, someArray)
+    console.log(`\tsomeArray.length = `, someArray.length)
+    console.log(`\tsomeArray[${someArray.length - 1}] = `, someArray[someArray.length - 1])
+
+    console.log('- Push')
+
+    let pushLength = someArray.push("New Element")
+    console.log(`\t$ let pushLength = someArray.push("New Element")`)
+    console.log('\tpushLength = ', pushLength)
+    console.log(`\tsomeArray.toString() = `, someArray.toString())
+    console.log(`\tsomeArray = `, someArray)
+    console.log(`\tsomeArray.length = `, someArray.length)
+    console.log(`\tsomeArray[${someArray.length - 1}] = `, someArray[someArray.length - 1])
+
+    someArray[someArray.length] = 'Another New Element'
+    console.log(`\t$ someArray[someArray.length] = 'Another New Element'`)
+    console.log(`\tsomeArray.toString() = `, someArray.toString())
+    console.log(`\tsomeArray = `, someArray)
+    console.log(`\tsomeArray.length = `, someArray.length)
+    console.log(`\tsomeArray[${someArray.length - 1}] = `, someArray[someArray.length - 1])
+
+    console.log('- Pop')
+    let popedElement = someArray.pop()
+    console.log(`\t$ let popedElement someArray.pop()`)
+    console.log('\tpopedElement = ', popedElement)
+    console.log(`\tsomeArray.toString() = `, someArray.toString())
+    console.log(`\tsomeArray = `, someArray)
+    console.log(`\tsomeArray.length = `, someArray.length)
+    console.log(`\tsomeArray[${someArray.length - 1}] = `, someArray[someArray.length - 1])
+
+
+    someArray[someArray.length - 1] = undefined
+    console.log(`\t$ someArray[someArray.length] = undefined`)
+    console.log(`\tsomeArray.toString() = `, someArray.toString())
+    console.log(`\tsomeArray = `, someArray)
+    console.log(`\tsomeArray.length = `, someArray.length)
+    console.log(`\tsomeArray[${someArray.length - 1}] = `, someArray[someArray.length - 1])
+
+    someArray[someArray.length - 1] = null
+    console.log(`\t$ someArray[someArray.length] = null`)
+    console.log(`\tsomeArray.toString() = `, someArray.toString())
+    console.log(`\tsomeArray = `, someArray)
+    console.log(`\tsomeArray.length = `, someArray.length)
+    console.log(`\tsomeArray[${someArray.length - 1}] = `, someArray[someArray.length - 1])
+
+    console.groupEnd()
+}
+
+function demoArray4() {
+    console.group("Shift & Unshift")
+
+    const someArray = ["string", 123, "1324", true, null, undefined, NaN, Infinity, 0, 'c']
+    console.log(`\t$ const someArray = [ "string", 123, "1324", true, null, undefined, NaN, Infinity, 0, 'c']`)
+    console.log(`\tsomeArray.toString() = `, someArray.toString())
+    console.log(`\tsomeArray = `, someArray)
+    console.log(`\tsomeArray.length = `, someArray.length)
+    console.log(`\tsomeArray[${someArray.length - 1}] = `, someArray[someArray.length - 1])
+
+    console.log('- Unshift')
+
+    let unshiftResult = someArray.unshift('New Element')
+    console.log(`\t$ let unshiftResult = someArray.unshift('New Element')`)
+    console.log('\tunshiftResult = ', unshiftResult)
+    console.log(`\tsomeArray.toString() = `, someArray.toString())
+    console.log(`\tsomeArray = `, someArray)
+    console.log(`\tsomeArray.length = `, someArray.length)
+    console.log(`\tsomeArray[0] = `, someArray[0])
+
+    console.log('- Shift')
+    let shiftedElement = someArray.shift()
+    console.log(`\t$ let shiftedElement = someArray.shift()`)
+    console.log('\tshiftedElement = ', shiftedElement)
+    console.log(`\tsomeArray.toString() = `, someArray.toString())
+    console.log(`\tsomeArray = `, someArray)
+    console.log(`\tsomeArray.length = `, someArray.length)
+    console.log(`\tsomeArray[${someArray.length - 1}] = `, someArray[someArray.length - 1])
+
+    console.groupEnd()
+}
+
+function demoArray5() {
+    console.group("Concat")
+
+    let someArray = ['red', 'green', 'blue']
+    console.log(`$ let someArray = ['red', 'green', 'blue']\n\tsomeArray = `, someArray)
+
+    let secondArray = ['Cat', 'Dog', 'Tiger']
+    console.log(`$ let secondArray = ['Cat', 'Dog', 'Tiger']\n\tsecondArray = `, secondArray)
+
+    let thirdArray = someArray.concat(secondArray), fourthArray = secondArray.concat(someArray)
+    console.log('$ let thirdArray = someArray.concat(secondArray), fourthArray = secondArray.concat(someArray)')
+    console.log('\tsomeArray = ', someArray)
+    console.log('\tsecondArray = ', secondArray)
+    console.log('\tthirdArray = ', thirdArray)
+    console.log('\tfourthArray = ', fourthArray)
+
+    console.groupEnd()
+}
+
+function demoArray6() {
+    console.group("Reverse")
+
+    let someArray = ['red', 'green', 'blue']
+    console.log(`$ let someArray = ['red', 'green', 'blue']\n\tsomeArray = `, someArray)
+
+    let newArray = someArray.reverse()
+    console.log(`$ let newArray = someArray.reverse()\n\tsomeArray = `, someArray, `\n\tnewArray = `, newArray)
+
+    console.log(`$ someArray == newArray\n\t${someArray == newArray}`)
+
+    console.groupEnd()
+}
+
+function demoArray7() {
+    console.group("IndexOf & Includes")
+
+    let someArray = ['red', 'green', 'blue']
+    console.log(`$ let someArray = ['red', 'green', 'blue']\n\tsomeArray = `, someArray)
+
+    console.log(`$ someArray.includes('red')\n\t${someArray.includes('red')}`)
+    console.log(`$ someArray.indexOf('red')\n\t${someArray.indexOf('red')}`)
+
+    console.log(`$ someArray.includes('dog')\n\t${someArray.includes('dog')}`)
+    console.log(`$ someArray.indexOf('dog')\n\t${someArray.indexOf('dog')}`)
+
+    console.groupEnd()
+}
